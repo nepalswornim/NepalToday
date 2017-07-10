3f6ae634d0531945006f8e8a68fe7e560521b745
Swornim Nepal <swornim.nepal@braindigit.com>
2017-07-10 14:59:14 +0545

    changes

--- components/Index.js
+++ components/Index.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import { Text, View,Alert } from 'react-native';
-const Index = () => {
-    transform=()=>{
-console.log("sjdfjasdfasdjj")
+import React, {
+    Component
+} from 'react';
+import {
+    Text,
+    View,
+    Alert
+} from 'react-native';
+import SplashScreen from 'react-native-smart-splash-screen'
+class Index extends Component {
+    componentDidMount() {
+        //SplashScreen.close(SplashScreen.animationType.scale, 850, 500) 
+        SplashScreen.close({
+            animationType: SplashScreen.animationType.scale,
+            duration: 850,
+            delay: 500,
+        })
     }
-setTimeout(function() { this.transform; }, 5000);
-    return (
-<View style={styles.bgStyle}>
-    <Text >Index</Text>
-</View>
+    render() {
+        return (
 
-    );
-
-};
-const styles={
-bgStyle:{
-backgroundColor:'#DC143C',
-flex:1
-    
+            <View style = {styles.bgStyle} >
+            <Text> Index </Text> </View>
+        );
+    }
 }
-
+const styles = {
+    bgStyle: {
+        backgroundColor: '#DC143C',
+        flex: 1
+    }
 }
-
 export default Index;
-
