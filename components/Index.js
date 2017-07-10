9ab64a93d8716491e7cfd7f3be0760e5e6eb634e
Swornim Nepal <swornim.nepal@braindigit.com>
2017-07-10 16:50:56 +0545

    mm

--- components/Index.js
+++ components/Index.js
@@ -4,22 +4,15 @@ import SplashScreen from 'react-native-smart-splash-screen'
 import axios from 'axios';
 class Index extends Component {
      state = {
-        Curd: null,
-        dates:[]
+     
+        dates:[],
+        //    Curd: null,
     }
-    config = {
-        apiKey: "AIzaSyBLQ1N7fhzGWYyiSuQufbCf5oxq4ct56DA",
-        authDomain: "ndcli-1819a.firebaseapp.com",
-        databaseURL: "https://ndcli-1819a.firebaseio.com",
-        projectId: "ndcli-1819a",
-        storageBucket: "ndcli-1819a.appspot.com",
-        messagingSenderId: "190440955820"
-    };
-    firebase = firebase.initializeApp(this.config);
-    database = firebase.database().ref();
+
    
     componentDidMount() {
-
+        var response=   axios.get("https://ndcli-1819a.firebaseio.com/.json");
+        console
         //SplashScreen.close(SplashScreen.animationType.scale, 850, 500) 
         //fade scale none
         //close(animationType, duration, delay) close splash screen with custom animation
@@ -31,18 +24,18 @@ class Index extends Component {
             duration: 850,
             delay: 500,
         })
-        setInterval(() => {
-            this.setState({
-                Curd: new Date().toLocaleString()
-            })
-        }, 1000)
-           axios.get("https://ndcli-1819a.firebaseio.com/.json").then(response=>this.setState({dates  : response.data}));
+        // setInterval(() => {
+        //     this.setState({
+        //         Curd: new Date().toLocaleString()
+        //     })
+        // }, 1000)
+
      console.log(this.state.dates)
     }
     render() {
 
         return ( <View style = {styles.bgStyle}>
-            <Text>{this.state.Curd}</Text></View>
+            <Text>vv</Text></View>
         );
     }
 }
